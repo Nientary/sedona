3607cd0185446261096785b5d75802d1cacf3db3
Nientary <Nientary@yandex.ru>
2017-06-16 15:58:19 +0300

    Скорректировала размеры всего

---
+++ js/menu.js
@@ -0,0 +1,4 @@
+var pageHeader = document.querySelector(".page-header");
+var menuToggle = document.querySelector(".page-header__toggle");
+
+pageHeader.classList.remove("page-header--no-js"); //открытие-закрытие тоже сделаю, но чуть позже
