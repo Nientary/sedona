84f0b3bfeba684c3e63a6b42079c87d28669b8bd
Nientary <Nientary@yandex.ru>
2017-06-18 19:54:50 +0300

    Графика и js

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,10 @@
 var pageHeader = document.querySelector(".page-header");
 var menuToggle = document.querySelector(".page-header__toggle");
 
-pageHeader.classList.remove("page-header--no-js"); //открытие-закрытие тоже сделаю, но чуть позже
+pageHeader.classList.remove("page-header--no-js");
+
+menuToggle.addEventListener("click", function(event) {
+  event.preventDefault();
+  pageHeader.classList.toggle("page-header--menu-opened");
+  pageHeader.classList.toggle("page-header--menu-closed");
+});
