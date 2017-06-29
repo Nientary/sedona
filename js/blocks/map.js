705ff02810ba2c1d56c69eacf4ea1f3f05c6f060
Nientary <Nientary@yandex.ru>
2017-06-30 02:53:12 +0300

    commit

--- js/blocks/map.js
+++ js/blocks/map.js
@@ -6,7 +6,7 @@ function initialize() {
   }
   var map = new google.maps.Map(document.getElementById("map-canvas"),
                                 mapOptions);
-  var image = "../img/icon-map.png";
+  var image = "img/icon-map-marker.svg";
   var myLatLng = new google.maps.LatLng(34.8697809, -111.7609631);
   var marker = new google.maps.Marker({
     position: myLatLng,
